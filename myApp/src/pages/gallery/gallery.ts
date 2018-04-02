2d582f2ce9e354bb06e205260fa6d09779b32fa4
Sun, Shaoxia <shaoxia.sun@accenture.com>
2018-04-02 19:10:01 +0800

    add gallery tab

--- myApp/src/pages/gallery/gallery.ts
+++ myApp/src/pages/gallery/gallery.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PictureDetailPage } from "../picturedetail/picturedetail"
 
 @Component({
   selector: 'page-gallery',
   templateUrl: 'gallery.html'
 })
 export class GalleryPage {
+  Items = [];
 
   constructor(public navCtrl: NavController) {
+    this.Items.push({
+      imgsrc: "love.jpg",
+      city: "Shanghai",
+      date: "2015/6/1"
+    },
+      {
+        imgsrc: "xiamen.jpg",
+        city: "Xiamen",
+        date: "2015/2/14"
+      },
+      {
+        imgsrc: "shenzhen.jpg",
+        city: "Shenzhen",
+        date: "2015/6/1"
+      },
+      {
+        imgsrc: "sanya.jpg",
+        city: "Sanya",
+        date: "2015/6/1"
+      },
+      {
+        imgsrc: "dubi.jpg",
+        city: "Dubi",
+        date: "2015/6/1"
+      }
+    )
 
+  }
+  openPictureDetailsPage(item) {
+    this.navCtrl.push(PictureDetailPage, { item: item });
   }
 
 }
--- myApp/src/pages/picturedetail/picturedetail.ts
+++ myApp/src/pages/picturedetail/picturedetail.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
 
 @Component({
   selector: 'page-picturedetail',
   templateUrl: 'picturedetail.html'
 })
 export class PictureDetailPage {
-
-  constructor(public navCtrl: NavController) {
-
+  Item;
+  constructor(public navCtrl: NavController, navParams: NavParams) {
+    var obj = navParams.data.item;
+    switch (obj.city) {
+      case "Shanghai":
+        this.Item = {
+          img: "love.jpg",
+          title: "Happy Birthday to You",
+          description: "This distaff romantic tear jerker concerns a young couple who fall in love despite the objections of their families. Taken from Erich Segal's best selling novel, Jenny (Ali MacGraw) is the Radcliffe student of modest means who has worked hard to excel academically. Oliver Barrett IV (Ryan O'Neal) is the son of a wealthy but coldhearted father (Ray Milland)."
+        };
+        break;
+      case "Shenzhen":
+        this.Item = {
+          img: "shenzhen.jpg",
+          title: "Nice to meet you",
+          description: "This distaff romantic tear jerker concerns a young couple who fall in love despite the objections of their families. Taken from Erich Segal's best selling novel, Jenny (Ali MacGraw) is the Radcliffe student of modest means who has worked hard to excel academically. Oliver Barrett IV (Ryan O'Neal) is the son of a wealthy but coldhearted father (Ray Milland)."
+        };
+        break;
+        default:
+        this.Item = {
+          img: "sanya.jpg",
+          title: "Happy New Year",
+          description: "I was in the audience at Lane Tech College Prep on Tuesday covering the event. As a former special educator –and special education student — I watched with keen interest as Apple told their story around education. While Apple is targeting the mainstream, I came away with strong impressions on how Apple can make serious inroads in furthering special education as well."
+        };
+    }
   }
 
 }
